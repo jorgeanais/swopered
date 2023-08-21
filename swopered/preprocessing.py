d63fb48f70e50578b1426a390267a65bbb61578c
Jorge Anais <jrganais@gmail.com>
2023-08-20 23:49:19 -0400

    Astropytable created from fits files

--- swopered/preprocessing.py
+++ swopered/preprocessing.py
@@ -1,38 +1,41 @@
-import astropy.io.fits as fits
 from glob import glob
 from pathlib import Path
+
+import astropy.io.fits as fits
 from astropy.table import Table
 
 
-def create_table_from_images(data_dir: Path) -> Table:
+def create_table_from_fits(data_dir: Path) -> Table:
     """
     This function reads a directory where fits images are expected to be. 
     From these files it constructs a table using the header information about the images.
     """
 
+    df_data = []
 
-    for fits_file in data_dir.glob("*.fits"):
+    for fits_file in sorted(data_dir.glob("*.fits")):
         with fits.open(fits_file) as hdul:
-            hdu = hdul[0]
-            header = hdu.header
+                hdu = hdul[0]
+                header = hdu.header
+        
+        df_data.append({
+                "file": header["FILENAME"][:-2],
+                "quadrant": header["FILENAME"][-2:],
+                "exptype": header["EXPTYPE"],
+                "object": header["OBJECT"],
+                "exptime": header["EXPTIME"],
+                "filter": header["FILTER"],
+                "ra": header["RA"],
+                "dec": header["DEC"],
+                "dec_d": header["DEC-D"],
+                "ra_d": header["RA-D"],
+                "equinox": header["EQUINOX"],
+                "telfocus": header["TELFOCUS"],
+                "airmass": header["AIRMASS"],
+                "ut_date": header["UT-DATE"],
+                "ut_time": header["UT-TIME"],
+                "ut_end": header["UT-END"],
+                "path": str(fits_file),
+            })
 
-        data = {
-            "filename": header["FILENAME"],
-            "exptype": header["EXPTYPE"],
-            "object": header["OBJECT"],
-            "exptime": header["EXPTIME"],
-            "filter": header["FILTER"],
-            "ra": header["RA"],
-            "ra_d": header["RA-D"],
-            "dec": header["DEC"],
-            "dec_d": header["DEC-D"],
-            "equinox": header["EQUINOX"],
-            "telfocus": header["TELFOCUS"],
-            "airmass": header["AIRMASS"],
-            "ut_date": header["UT-DATE"],
-            "ut_time": header["UT-TIME"],
-            "ut_end": header["UT-END"],
-            "path": fits_file.resolve(),
-        }
-
-    return Table(data)
+    return Table(df_data)
